a48b35c5e963c8c0de9b9882fa59f46749a6b6b5
Ludwig <theludway@gmail.com>
2024-11-19 23:14:14 -0500

    Empezo esto

---
+++ Aprendizaje/FreeCodeCamp/Comments/main.cpp
@@ -0,0 +1,8 @@
+// Esto trae a la libreria iostream
+#include <iostream>
+
+auto main(int argc, char *argv[]) -> int {
+    // Imprime Hola mundo a la consola
+    std::cout << "Hola mundo" << std::endl;
+    return 0;
+}
---
+++ Aprendizaje/FreeCodeCamp/FirstProgram/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+// Funcion main
+int main() {
+
+    for (int i=0;i <10 ; i++) {
+        std::cout << "Ludwig Alvarado" << std::endl;
+    }
+
+    std::cout << "Ludwig Alvarado" << std::endl;
+    std::cout << "Ludwig Alvarado" << std::endl;
+    std::cout << "Ludwig Alvarado" << std::endl;
+    std::cout << "Ludwig Alvarado" << std::endl;
+    std::cout << "Ludwig Alvarado" << std::endl;
+    std::cout << "Ludwig Alvarado" << std::endl;
+    std::cout << "Ludwig Alvarado" << std::endl;
+    std::cout << "Ludwig Alvarado" << std::endl;
+    std::cout << "Ludwig Alvarado" << std::endl;
+    std::cout << "Ludwig Alvarado" << std::endl;
+    std::cout << "Ludwig Alvarado" << std::endl;
+
+    return 0;
+}

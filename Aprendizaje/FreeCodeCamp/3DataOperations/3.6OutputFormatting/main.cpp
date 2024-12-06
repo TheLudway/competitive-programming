6a096f5aec7dc0e4f2271760fb4a73d013cdb30d
Ludwig <theludway@gmail.com>
2024-12-06 10:51:10 -0500

    Formatting output

--- Aprendizaje/FreeCodeCamp/3DataOperations/3.6OutputFormatting/main.cpp
+++ Aprendizaje/FreeCodeCamp/3DataOperations/3.6OutputFormatting/main.cpp
@@ -88,5 +88,64 @@ int main(){
     std::cout << std::setw(col_width) << "Josh" << std::setw(col_width) << "Carr" << std::setw(5) << "27" << std::endl;
     std::cout << std::setw(col_width) << "Izaiah" << std::setw(col_width) << "Robinson" << std::setw(5) << "29" << std::endl;
 
+    std::cout << std::endl;
+    std::cout << "Tabla justificada a la izquierda : " << std::endl;
+
+    std::cout << std::left;
+    std::cout << std::setw(col_width) << "Apellido" << std::setw(col_width) << "Nombre" << std::setw(5) << "Edad" << std::endl;
+    std::cout << std::setw(col_width) << "Daniel" << std::setw(col_width) << "Gray" << std::setw(5) << "25" << std::endl;
+    std::cout << std::setw(col_width) << "Stanley" << std::setw(col_width) << "Woods" << std::setw(5) << "33" << std::endl;
+    std::cout << std::setw(col_width) << "Jordan" << std::setw(col_width) << "Parker" << std::setw(5) << "45" << std::endl;
+    std::cout << std::setw(col_width) << "Joe" << std::setw(col_width) << "Ball" << std::setw(5) << "21" << std::endl;
+    std::cout << std::setw(col_width) << "Josh" << std::setw(col_width) << "Carr" << std::setw(5) << "27" << std::endl;
+    std::cout << std::setw(col_width) << "Izaiah" << std::setw(col_width) << "Robinson" << std::setw(5) << "29" << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "Justificado interno : " << std::endl;
+    std::cout << std::right;
+    std::cout << std::setw(10) << -123.45 << std::endl;
+    std::cout << std::internal;
+    std::cout << std::setw(10) << -123.45 << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "--------------- boolalpha ---------------" << std::endl;
+
+    bool condition {true};
+    bool other_condition {false};
+
+    std::cout << "condition : " << condition << std::endl;
+    std::cout << "other_condition : " << other_condition << std::endl;
+    
+    std::cout << std::endl;
+    std::cout << std::boolalpha;
+    std::cout << "condition : " << condition << std::endl;
+    std::cout << "other_condition : " << other_condition << std::endl;
+
+    std::cout << std::endl;
+    std::cout << std::noboolalpha;
+    std::cout << "contigion : " << condition << std::endl;
+    std::cout << "other_condition : " << other_condition << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "--------------- showpos ---------------" << std::endl;
+
+    int pos_num {34};
+    int neg_num {-45};
+
+    std::cout << "pos_num : " << pos_num << std::endl;
+    std::cout << "neg_num : " << neg_num << std::endl;
+
+    std::cout << std::endl;
+    std::cout << std::showpos;
+    std::cout << "pos_num : " << pos_num << std::endl;
+    std::cout << "neg_num : " << neg_num << std::endl;
+
+    std::cout << std::endl;
+    std::cout << std::noshowpos;
+    std::cout << "pos_num : " << pos_num << std::endl;
+
+
+
+
     return 0;
 }

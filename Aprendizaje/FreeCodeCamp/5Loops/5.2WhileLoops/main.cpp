8c89917fb510b46a26f9a83f85d7ec2508f19cd6
Ludwig <theludway@gmail.com>
2024-12-13 05:30:45 -0500

    While Loop

---
+++ Aprendizaje/FreeCodeCamp/5Loops/5.2WhileLoops/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+    const unsigned int COUNT {10};
+    unsigned int i {0};
+
+    while (i < COUNT) {
+        std::cout << i << " Amo C++" << std::endl;
+        i++;
+    }
+
+
+
+    return 0;
+}

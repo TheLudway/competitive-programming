1654c8a9cfe568c7232df618624a87bdca986376
Ludwig <theludway@gmail.com>
2024-12-14 01:56:17 -0500

    Commentarions

--- Aprendizaje/FreeCodeCamp/5Loops/5.3DoWhileLoops/exercises.cpp
+++ Aprendizaje/FreeCodeCamp/5Loops/5.3DoWhileLoops/exercises.cpp
@@ -4,41 +4,41 @@
 int main(){
 
 
-    // std::cout << "Suma de los numeros del 1 al 100" << std::endl;
-    // const int COUNT1 {100};
-    // size_t i1 {1};
-    // int r1 {0};
+    std::cout << "Suma de los numeros del 1 al 100" << std::endl;
+    const int COUNT1 {100};
+    size_t i1 {1};
+    int r1 {0};
 
-    // for(i1 ; i1 <= COUNT1 ; i1++){
-    //     r1 += i1;
-    // }
+    for(i1 ; i1 <= COUNT1 ; i1++){
+        r1 += i1;
+    }
 
-    // std::cout << "La suma desde " << i1 << " hasta " << COUNT1 << " es " << r1 << std::endl;
+    std::cout << "La suma desde " << i1 << " hasta " << COUNT1 << " es " << r1 << std::endl;
 
-    // std::cout << "Tabla de multplicacion" << std::endl;
-    // const int COUNT2 {10};
-    // size_t i2 {0};
-    // int n2_1 {};
-    // std::cout << "De cual numero quiere generar su tabla de multiplicar?" << std::endl;
-    // std::cin >> n2_1;
+    std::cout << "Tabla de multplicacion" << std::endl;
+    const int COUNT2 {10};
+    size_t i2 {0};
+    int n2_1 {};
+    std::cout << "De cual numero quiere generar su tabla de multiplicar?" << std::endl;
+    std::cin >> n2_1;
 
-    // for (i2 ; i2 <= COUNT2 ; i2++){
-    //     std::cout << n2_1 << " X " << i2 << " = " << n2_1*i2 << std::endl;
-    // }
+    for (i2 ; i2 <= COUNT2 ; i2++){
+        std::cout << n2_1 << " X " << i2 << " = " << n2_1*i2 << std::endl;
+    }
 
-    // std::cout << "Calculadora de factorial" << std::endl;
-    // size_t i3 {1};
-    // unsigned int n3_1 {};
-    // unsigned int r3 {1};
+    std::cout << "Calculadora de factorial" << std::endl;
+    size_t i3 {1};
+    unsigned int n3_1 {};
+    unsigned int r3 {1};
 
-    // std::cout << "Escribe el num para sacar el factorial" << std::endl;
-    // std::cin >> n3_1;
+    std::cout << "Escribe el num para sacar el factorial" << std::endl;
+    std::cin >> n3_1;
 
-    // while (i3 <= n3_1){
-    //     r3 *= i3;
-    //     i3++;
-    // }
-    // std::cout << "El factorial de " << n3_1 << " es " << r3 << std::endl;
+    while (i3 <= n3_1){
+        r3 *= i3;
+        i3++;
+    }
+    std::cout << "El factorial de " << n3_1 << " es " << r3 << std::endl;
 
     std::random_device rd;
     std::mt19937 gen(rd());

4715abc91a6c74c8c72db0a6cc07bf12cdee10c1
Ludwig <theludway@gmail.com>
2024-12-25 15:59:05 -0500

    New Fails

--- Aprendizaje/FreeCodeCamp/7Pointers/7.5NewFails/main.cpp
+++ Aprendizaje/FreeCodeCamp/7Pointers/7.5NewFails/main.cpp
@@ -8,6 +8,25 @@ int main(){
         int *lots_of_ints2 { new int [1000000]};
     }
 
+    // Excepciones
+    for (size_t i {0}; i < 100000000000; i ++){
+        try {
+            int* lots_of_ints3 { new int [10000000]};
+        }catch(std::exception& ex){
+            std::cout << "Excepcion : " << ex.what() << std::endl;
+        }
+    }
+
+    // std::nothrow
+    for (size_t i {0} ; i < 100 ; i ++){
+        int * data = new (std::nothrow) int [10000000000];
+        if (data!= nullptr){
+            std::cout << "Datos almacenados\n";
+        }else{
+            std::cout << "Almacenamiento de datos fallido\n";
+        }
+    }
+
 
     return 0;
 }

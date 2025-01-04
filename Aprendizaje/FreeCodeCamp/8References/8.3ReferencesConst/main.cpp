df76483d0937a7c945f80c57ddb46feae71fb50e
Ludwig <theludway@gmail.com>
2025-01-04 00:52:14 -0500

    References const

---
+++ Aprendizaje/FreeCodeCamp/8References/8.3ReferencesConst/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main(){
+
+    // Referencia no constante
+    std::cout << "\n";
+    std::cout << "Referencia no constante : \n";
+    int age {27};
+    int& ref_age {age};
+
+    std::cout << "age : " << age << "\n";
+    std::cout << "ref_age : " << ref_age << "\n";
+
+    // Se puede modificar la variable original a traves de la referencia
+    std::cout << "\n";
+    std::cout << "Modificar la variable original a traves de la referencia : \n";
+
+    ref_age++;
+
+    std::cout << "age : " << age << "\n";
+    std::cout << "ref_age : " << ref_age << "\n";
+
+
+
+
+    return 0;
+}

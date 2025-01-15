1ef1548a284c9415dca45efcdb5506f34b7e7fc2
TheLudway <theludway@gmail.com>
2025-01-14 20:56:45 -0500

    Manage multiple functions in multiple files

--- Aprendizaje/FreeCodeCamp/11Functions/11.3FunctionsMultipleFiles/main.cpp
+++ Aprendizaje/FreeCodeCamp/11Functions/11.3FunctionsMultipleFiles/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "compare.h" // Preprocesamiento
+#include "operations.h"
 #define endl "\n";
+
 
 
 
@@ -8,9 +10,11 @@ int main(){
 
     int max_num = max(34, 56);
     int min_num = min(34, 56);
+    int incr_mult_num = incr_mult(2, 3);
 
     std::cout << "max : " << max_num << endl;
     std::cout << "min : " << min_num << endl;
+    std::cout << "incr_mult_num : " << incr_mult_num << endl;
 
     return 0;
 }

4b0b68db5ae2ae4ff3b5826a64cf45fa79f100b7
TheLudway <theludway@gmail.com>
2025-01-24 20:36:54 -0500

    Lambda function

--- Aprendizaje/FreeCodeCamp/12LambdaFunctions/12.1DeclareAndUse/main.cpp
+++ Aprendizaje/FreeCodeCamp/12LambdaFunctions/12.1DeclareAndUse/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <string>
 #define endl "\n"
 
+
 int main(){
+
+    [](){
+        std::cout << "Holi" << endl;
+    }();
+
+    [](double a, double b){
+        std::cout << "a + b : " << (a + b) << endl;
+    }(12.1, 5.7);
+
+    auto result = [](double a, double b)-> double{
+        return (a + b);
+    }(12.1, 5.7);
+    std::cout << "result : " << result << endl;
+
+
 
 
     return 0;
